f63a3afb59312b3d9731d427ab9f42e55d36c9a9
Jai Agrawal <153916803+agrawaljai@users.noreply.github.com>
2024-07-02 03:00:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Board from './TicTacToe/Board';
+import Title from './TicTacToe/title';
+
+function App() {
+  return (
+    <div className="App">
+      <Title />
+      <Board />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/TicTacToe/title.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Title = () => {
+    return (
+        <h1 className='title'>TIC-TAC-TOE</h1>
+    );
+
+}
+
+export default Title;
